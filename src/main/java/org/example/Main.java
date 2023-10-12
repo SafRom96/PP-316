799c7f193c7df4ee8706ec7185e5ee18dbecd6b8
ntc <savinr95@gmail.com>
2023-10-12 10:43:14 +0300

    feat: "добавил конкатенацию"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.config.MyConfig;
+import org.example.entity.User;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+
+        System.out.println(communication.save(new User(3L, "James", "Brown", (byte) 26)) +
+                communication.update(new User(3L, "Thomas", "Shelby", (byte) 26)) + communication.delete(3L));
     }
 }
